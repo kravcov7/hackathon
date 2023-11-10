2b66297ad3092a208144e98bff2f3ff527445a43
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-11-10 19:55:58 +0100

    initial commit

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api/python")
+
+@app.get("/api/user")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"data": {"id": "01", "name": "John Doe"}}
