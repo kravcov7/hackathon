8ee544d98c380e47dab14d2974593b92f2cf853f
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-11-10 20:06:26 +0100

    two endpoints added

--- api/index.py
+++ api/index.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api/python")
+
+@app.get("/api/login")
+def hello_world():
+    return {"data": {"id": "01", "name": "John Doe"}}
+
+
+@app.get("/api/signup")
 def hello_world():
     return {"message": "Hello World"}
