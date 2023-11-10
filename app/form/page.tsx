210fc928b3b38c830858d030ce385f49c3011b84
Elvira Krutova <77353395+Elvira-del@users.noreply.github.com>
2023-11-10 17:26:05 +0000

    feat: add route for signup form

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -2,10 +2,5 @@ import LoginForm from "../ui/login-form";
 import SignupForm from "../ui/signup-form";
 
 export default function Page() {
-  return (
-    <>
-      <LoginForm />;
-      <SignupForm />
-    </>
-  );
+  return <LoginForm />;
 }
--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -37,7 +37,7 @@ export default function SignupForm() {
       </label>
       <button
         type="submit"
-        className="py-2 px-5 rounded-lg bg-indigo-500 text-white"
+        className="py-2 px-5 rounded-lg bg-emerald-600 text-white"
       >
         Submit
       </button>
